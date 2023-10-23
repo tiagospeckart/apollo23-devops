d7ff7d304aad52a8f3ec83d1952966c4ccdea232
Everton-WS <everton@doctor.com>
2023-10-23 10:35:14 -0300

    create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+# Este output fornece o endereço IP público da instância EC2
+output "instance_ip" {
+  value = aws_instance.apollo23_hackweek_vm.public_ip
+  description = "O IP público da instância EC2"
+}
+
+# Este output fornece o ID da instância EC2
+output "instance_id" {
+  value = aws_instance.apollo23_hackweek_vm.id
+}
+
+# Este output fornece o nome de domínio público da instância EC2
+output "instance_public_dns" {
+  value = aws_instance.apollo23_hackweek_vm.public_dns
+}
